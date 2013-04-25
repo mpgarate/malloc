b287dc4781830bf68c0c591e8d12abbcaf61006e
Michael Garate <mpgarate@gmail.com>
2013-04-25 19:53:20 -0400

    Initialize the project

---
+++ mm.c
@@ -0,0 +1,113 @@
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ * 
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+/*********************************************************
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * replace the first three fields in the following struct
+ * with your own information (you may choose your own
+ * "team name").  Leave the last two feilds blank.
+ ********************************************************/
+team_t team = {
+	/* Team name */
+	"instructors",
+	/* First member's full name */
+	"Michael Garate",
+	/* First member's NYU NetID*/
+	"mg3626@nyu.edu",
+	/* Second member's full name (leave blank if none) */
+	"",
+	/* Second member's email address (leave blank if none) */
+	""
+};
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* 
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void)
+{
+	return 0;
+}
+
+/* 
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void *mm_malloc(size_t size)
+{
+	int newsize = ALIGN(size + SIZE_T_SIZE);
+	void *p = mem_sbrk(newsize);					//tries to increase size of heap by [newsize]
+	if (p == (void *)-1)
+		return NULL;								//return null if fails
+	else {
+		*(size_t *)p = size;
+		return (void *)((char *)p + SIZE_T_SIZE);	//if successful returns pointer
+	}
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr)
+{
+}
+
+/*
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+  //I am pointing to some space and I need to make it bigger
+ */
+void *mm_realloc(void *ptr, size_t size)
+{
+	void *oldptr = ptr;
+	void *newptr;
+	size_t copySize;
+
+	newptr = mm_malloc(size);
+	if (newptr == NULL)
+		return NULL;
+	copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+	if (size < copySize)
+		copySize = size;
+	memcpy(newptr, oldptr, copySize);
+	mm_free(oldptr);
+	return newptr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
